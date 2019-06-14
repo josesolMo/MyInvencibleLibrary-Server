4c666186e228b99f58367ee052d15572528ed1ec
Rubén Salas <rubsalas@hotmail.com>
2019-06-14 16:32:17 -0600

    Pruebas para la graficacion de tabla en cliente

---
+++ ServerController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ruben on 14/06/19.
+//
+
+#include "ServerController.h"
--- main.cpp
+++ main.cpp
@@ -41,6 +41,12 @@ static string JSONReturnDescripcion;
 json_object *jObj = json_object_new_object();
 
 
+/**
+ *
+ * @param jObj
+ * @param destinatary
+ * @return
+ */
 int sendJSON(json_object *jObj, string destinatary) {
 
     ///Guarda el ip que se utilizara para la comunicacion
@@ -128,6 +134,15 @@ int sendJSON(json_object *jObj, string destinatary) {
 
     ///KEYS PARA GRAFICAR TABLA
 
+
+    ///KEY: FILE_ID
+    struct json_object *tempFile_Id;
+    json_object *parsed_jsonFile_Id = json_tokener_parse(recvBuff);
+    json_object_object_get_ex(parsed_jsonFile_Id, "FILE_ID", &tempFile_Id);
+    if (json_object_get_int(tempFile_Id) != 0) {
+        ///Variable por guardar o funcion por llamar
+        JSONReturnGen = json_object_get_string(tempFile_Id);
+    }
 
     ///KEY: NOMBRE
     struct json_object *tempNombre;
@@ -304,10 +319,163 @@ string sendConsole(string console, string consoleKey) {
     ///Toma el key de la nueva imagen
     json_object_object_add(jObj,consoleKey.c_str(), jstringConsole);
 
+    sendJSON(jObj, "MetadataDB");
+
     ///Se manda el texto de la consola en el cliente a la clase SQLController
     sqlController->setStringToRead(console);
 
     ///Verificacion
+
+    return json_object_to_json_string(jObj);
+
+}
+
+
+string sendTest(string test, string testKey) {
+
+    ///KEY para probar la graficacion de cliente
+    string keyForTest = "CONSOLE";
+
+    ///Redefine el jObject para eliminar sus keys antiguos
+    jObj = json_object_new_object();
+
+
+    ///Instancia de jarray para pruebas General
+    json_object *jarrayTestGeneral = json_object_new_array();
+
+
+    ///Instancia de jarray para pruebas 0
+    json_object *jarrayTest0 = json_object_new_array();
+    ///Crea el titulo y la informacion de la columna 0
+    json_object *jstringTestColumna0 = json_object_new_string("FILE_ID");
+    json_object *jstringTest01 = json_object_new_string("viaje04.bmp");
+    json_object *jstringTest02 = json_object_new_string("playa.bmp");
+    json_object *jstringTest03 = json_object_new_string("gato.bmp");
+    json_object *jstringTest04 = json_object_new_string("reloj.bmp");
+    ///Para agregar al arrayTest0
+    json_object_array_add(jarrayTest0, jstringTestColumna0);
+    json_object_array_add(jarrayTest0, jstringTest01);
+    json_object_array_add(jarrayTest0, jstringTest02);
+    json_object_array_add(jarrayTest0, jstringTest03);
+    json_object_array_add(jarrayTest0, jstringTest04);
+
+
+    ///Instancia de jarray para pruebas 1
+    json_object *jarrayTest1 = json_object_new_array();
+    ///Crea el titulo y la informacion de la columna 1
+    json_object *jstringTestColumna1 = json_object_new_string("NOMBRE");
+    json_object *jstringTest11 = json_object_new_string("Imagen1");
+    json_object *jstringTest12 = json_object_new_string("Imagen2");
+    json_object *jstringTest13 = json_object_new_string("Imagen3");
+    json_object *jstringTest14 = json_object_new_string("Imagen4");
+    ///Para agregar al arrayTest1
+    json_object_array_add(jarrayTest1, jstringTestColumna1);
+    json_object_array_add(jarrayTest1, jstringTest11);
+    json_object_array_add(jarrayTest1, jstringTest12);
+    json_object_array_add(jarrayTest1, jstringTest13);
+    json_object_array_add(jarrayTest1, jstringTest14);
+
+
+    ///Instancia de jarray para pruebas 2
+    json_object *jarrayTest2 = json_object_new_array();
+    ///Crea el titulo y la informacion de la columna 2
+    json_object *jstringTestColumna2 = json_object_new_string("AUTOR");
+    json_object *jstringTest21 = json_object_new_string("Andrey");
+    json_object *jstringTest22 = json_object_new_string("Edgar");
+    json_object *jstringTest23 = json_object_new_string("José");
+    json_object *jstringTest24 = json_object_new_string("Ruben");
+    ///Para agregar al arrayTest2
+    json_object_array_add(jarrayTest2, jstringTestColumna2);
+    json_object_array_add(jarrayTest2, jstringTest21);
+    json_object_array_add(jarrayTest2, jstringTest22);
+    json_object_array_add(jarrayTest2, jstringTest23);
+    json_object_array_add(jarrayTest2, jstringTest24);
+
+
+    ///Instancia de jarray para pruebas 3
+    json_object *jarrayTest3 = json_object_new_array();
+    ///Crea el titulo y la informacion de la columna 3
+    json_object *jstringTestColumna3 = json_object_new_string("CREACION");
+    json_object *jstringTest31 = json_object_new_string("2004");
+    json_object *jstringTest32 = json_object_new_string("2013");
+    json_object *jstringTest33 = json_object_new_string("1994");
+    json_object *jstringTest34 = json_object_new_string("2019");
+    ///Para agregar al arrayTest3
+    json_object_array_add(jarrayTest3, jstringTestColumna3);
+    json_object_array_add(jarrayTest3, jstringTest31);
+    json_object_array_add(jarrayTest3, jstringTest32);
+    json_object_array_add(jarrayTest3, jstringTest33);
+    json_object_array_add(jarrayTest3, jstringTest34);
+
+
+    ///Instancia de jarray para pruebas 4
+    json_object *jarrayTest4 = json_object_new_array();
+    ///Crea el titulo y la informacion de la columna 4
+    json_object *jstringTestColumna4 = json_object_new_string("TAMANO");
+    json_object *jstringTest41 = json_object_new_string("1200x800");
+    json_object *jstringTest42 = json_object_new_string("250x250");
+    json_object *jstringTest43 = json_object_new_string("600x600");
+    json_object *jstringTest44 = json_object_new_string("4200x3600");
+    ///Para agregar al arrayTest4
+    json_object_array_add(jarrayTest4, jstringTestColumna4);
+    json_object_array_add(jarrayTest4, jstringTest41);
+    json_object_array_add(jarrayTest4, jstringTest42);
+    json_object_array_add(jarrayTest4, jstringTest43);
+    json_object_array_add(jarrayTest4, jstringTest44);
+
+
+    ///Instancia de jarray para pruebas 5
+    json_object *jarrayTest5 = json_object_new_array();
+    ///Crea el titulo y la informacion de la columna 5
+    json_object *jstringTestColumna5 = json_object_new_string("DESCRIPCION");
+    json_object *jstringTest51 = json_object_new_string("Edgar");
+    json_object *jstringTest52 = json_object_new_string("Es");
+    json_object *jstringTest53 = json_object_new_string("Muy");
+    json_object *jstringTest54 = json_object_new_string("Cabezon");
+    ///Para agregar al arrayTest5
+    json_object_array_add(jarrayTest5, jstringTestColumna5);
+    json_object_array_add(jarrayTest5, jstringTest51);
+    json_object_array_add(jarrayTest5, jstringTest52);
+    json_object_array_add(jarrayTest5, jstringTest53);
+    json_object_array_add(jarrayTest5, jstringTest54);
+
+
+
+    /*
+    ///Instancia de jarray para pruebas
+    json_object *jarrayTest = json_object_new_array();
+
+    ///Crea el titulo y la informacion de la columna 0
+    json_object *jstringTestColumna0 = json_object_new_string("x");
+    json_object *jstringTest01 = json_object_new_string("x");
+    json_object *jstringTest02 = json_object_new_string("x");
+    json_object *jstringTest03 = json_object_new_string("x");
+    json_object *jstringTest04 = json_object_new_string("x");
+
+    ///Para agregar al arrayTest
+    json_object_array_add(jarrayTest, jstringTestColumna);
+    json_object_array_add(jarrayTest, jstringTest1);
+    json_object_array_add(jarrayTest, jstringTest2);
+    json_object_array_add(jarrayTest, jstringTest3);
+    json_object_array_add(jarrayTest, jstringTest4);
+    */
+
+
+
+    ///Para agregar al arrayTestGeneral
+    json_object_array_add(jarrayTestGeneral, jarrayTest0);
+    json_object_array_add(jarrayTestGeneral, jarrayTest1);
+    json_object_array_add(jarrayTestGeneral, jarrayTest2);
+    json_object_array_add(jarrayTestGeneral, jarrayTest3);
+    json_object_array_add(jarrayTestGeneral, jarrayTest4);
+    json_object_array_add(jarrayTestGeneral, jarrayTest5);
+
+
+    ///Toma el key de la nueva imagen
+    json_object_object_add(jObj, keyForTest.c_str(), jarrayTestGeneral);
+
+
+    cout << json_object_to_json_string(jObj) << endl;
 
     return json_object_to_json_string(jObj);
 
@@ -410,8 +578,15 @@ int runServer() {
             json_object *parsed_jsonConsole = json_tokener_parse(buff);
             json_object_object_get_ex(parsed_jsonConsole, "CONSOLE", &tempConsole);
 
+
             ///KEYS PARA GRAFICAR TABLA
 
+
+            ///KEY: FILE_ID
+            ///Obtiene un request
+            struct json_object *tempFile_Id;
+            json_object *parsed_jsonFile_Id = json_tokener_parse(buff);
+            json_object_object_get_ex(parsed_jsonFile_Id, "FILE_ID", &tempFile_Id);
 
             ///KEY: NOMBRE
             ///Obtiene un request
@@ -430,27 +605,24 @@ int runServer() {
             struct json_object *tempCreacion;
             json_object *parsed_jsonCreacion = json_tokener_parse(buff);
             json_object_object_get_ex(parsed_jsonCreacion, "CREACION", &tempCreacion);
-            if (json_object_get_int(tempCreacion) != 0) {
-                JSONReturnCreacion = json_object_get_string(tempCreacion);
-            }
 
             ///KEY: TAMANO
             ///Obtiene un request
             struct json_object *tempTamano;
             json_object *parsed_jsonTamano = json_tokener_parse(buff);
             json_object_object_get_ex(parsed_jsonTamano, "TAMANO", &tempTamano);
-            if (json_object_get_int(tempTamano) != 0) {
-                JSONReturnTamano = json_object_get_string(tempTamano);
-            }
 
             ///KEY: DESCRIPCION
             ///Obtiene un request
             struct json_object *tempDescripcion;
             json_object *parsed_jsonDescripcion = json_tokener_parse(buff);
             json_object_object_get_ex(parsed_jsonDescripcion, "DESCRIPCION", &tempDescripcion);
-            if (json_object_get_int(tempDescripcion) != 0) {
-                JSONReturnDescripcion = json_object_get_string(tempDescripcion);
-            }
+
+            ///KEY: TEST
+            ///Obtiene un request para
+            struct json_object *tempTest;
+            json_object *parsed_jsonTest = json_tokener_parse(buff);
+            json_object_object_get_ex(parsed_jsonTest, "TEST", &tempTest);
 
 
 
@@ -458,7 +630,6 @@ int runServer() {
             ///KEY: TEMPLATE
             ///Obtiene un request para
             struct json_object *tempZZ;
-            cout<<"ZZ"<<endl;
             json_object *parsed_jsonZZ = json_tokener_parse(buff);
             json_object_object_get_ex(parsed_jsonZZ, "ZZ", &tempZZ);
              */
@@ -502,31 +673,74 @@ int runServer() {
                 send(fd2, sendConsoleBack.c_str(), MAXDATASIZE, 0);
             }
 
-            ///Obtendra un request para obtener
-            ///Verifica que reciba los KEYS: NOMBRE
-            if (json_object_get_int(tempNombre) != 0) {
+            ///KEYS FOR CLIENT TABLE
+
+/*
+            ///Obtendra un request para obtener el o los file_id's de la base de datos
+            ///Verifica que reciba los KEYS: FILE_ID
+            if (json_object_get_string(tempFile_Id) != nullptr ) {
                 ///JSON saliente del servidor
-                //string variable = sendNombre(json_object_get_string(tempNombre), "NOMBRE");
+                string variable = sendFile_Id(json_object_get_string(tempFile_Id), "FILE_ID");
                 ///Envio al cliente
-                //send(fd2, variable.c_str(), MAXDATASIZE, 0);
+                send(fd2, variable.c_str(), MAXDATASIZE, 0);
             }
 
-            ///Obtendra un request para obtener
-            ///Verifica que reciba los KEYS: TEMPLATE
+            ///Obtendra un request para obtener el o los nombres de la base de datos
+            ///Verifica que reciba los KEYS: NOMBRE
+            if (json_object_get_string(tempNombre) != 0) {
+                ///JSON saliente del servidor
+                string variable = sendNombre(json_object_get_string(tempNombre), "NOMBRE");
+                ///Envio al cliente
+                send(fd2, variable.c_str(), MAXDATASIZE, 0);
+            }
+
+            ///Obtendra un request para obtener el o los autores de la base de datos
+            ///Verifica que reciba los KEYS: AUTOR
             if (json_object_get_string(tempAutor) != nullptr ) {
                 ///JSON saliente del servidor
-                //string variable = sendAType(json_object_get_string(tempAutor), "KEY");
+                string variable = sendAutor(json_object_get_string(tempAutor), "AUTOR");
                 ///Envio al cliente
-                //send(fd2, variable.c_str(), MAXDATASIZE, 0);
+                send(fd2, variable.c_str(), MAXDATASIZE, 0);
             }
 
+            ///Obtendra un request para obtener el o lo años de creacion de la base de datos
+            ///Verifica que reciba los KEYS: CREACION
+            if (json_object_get_string(tempCreacion) != 0) {
+                ///JSON saliente del servidor
+                string variable = sendCreacion(json_object_get_string(tempCreacion), "CREACION");
+                ///Envio al cliente
+                send(fd2, variable.c_str(), MAXDATASIZE, 0);
+            }
 
+            ///Obtendra un request para obtener el o los tamaños de la base de datos
+            ///Verifica que reciba los KEYS: TAMANO
+            if (json_object_get_string(tempTamano) != nullptr ) {
+                ///JSON saliente del servidor
+                string variable = sendTamano(json_object_get_string(tempTamano), "TAMANO");
+                ///Envio al cliente
+                send(fd2, variable.c_str(), MAXDATASIZE, 0);
+            }
 
+            ///Obtendra un request para obtener la o las descripciones de la base de datos
+            ///Verifica que reciba los KEYS: DESCRIPCION
+            if (json_object_get_string(tempDescripcion) != 0) {
+                ///JSON saliente del servidor
+                string variable = sendDescripcion(json_object_get_string(tempDescripcion), "DESCRIPCION");
+                ///Envio al cliente
+                send(fd2, variable.c_str(), MAXDATASIZE, 0);
+            }*/
 
-
-
+            ///Obtendra un request para obtener
+            ///Verifica que reciba los KEYS: TEST
+            if (json_object_get_string(tempTest) != nullptr ) {
+                ///JSON saliente del servidor
+                string test = sendTest(json_object_get_string(tempTest), "TEST");
+                ///Envio al cliente
+                send(fd2, test.c_str(), MAXDATASIZE, 0);
+            }
 
             /*
+
             ///Obtendra un request para obtener
             ///Verifica que reciba los KEYS: TEMPLATE
             if (json_object_get_string(tempZZ) != nullptr ) {
@@ -567,6 +781,13 @@ int main(){
 
     ///Corre el servidor
     runServer();
+
+
+    ///Imprime el Test
+    sendTest("","");
+
+
+
 
     return 0;
 }
